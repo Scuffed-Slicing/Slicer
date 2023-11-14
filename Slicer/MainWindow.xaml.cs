74e4cb2fc0d9aa2178cbad3e69b4aaea4983c33c
DenzellM-2054872 <denzell.mgbokwere@student.uhasselt.be>
2023-11-14 15:53:00 +0100

    models can now be rendered in the viewport

--- Slicer/MainWindow.xaml.cs
+++ Slicer/MainWindow.xaml.cs
@@ -10,8 +10,11 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using HelixToolkit.Wpf;
+
 
 namespace Slicer
 {
@@ -27,7 +30,47 @@ namespace Slicer
 
         private void ImportFile(object sender, RoutedEventArgs e)
         {
-            Console.WriteLine("i wanna kms");
+            Microsoft.Win32.OpenFileDialog explorer = new Microsoft.Win32.OpenFileDialog();
+            bool? result = explorer.ShowDialog();
+            
+            explorer.DefaultExt = ".stl"; // Default file extension
+            explorer.Filter = "Statement List (.stl)|*.stl"; // Filter files by extension
+            
+            // Process open file dialog box results 
+            
+            if (result == true)
+            {
+                // Open document
+                string filename = explorer.FileName;
+                Console.WriteLine(filename);
+                
+                StLReader reader = new StLReader();
+                Model3DGroup group = reader.Read(filename);
+                GeometryModel3D geometryModel = FindLargestModel(group);
+                MeshGeometry3D mesh = geometryModel.Geometry as MeshGeometry3D;
+                // access triangles
+                Int32Collection indices = mesh.TriangleIndices;
+
+                ModelVisual3D.Content = geometryModel;
+            }
+        }
+        private GeometryModel3D FindLargestModel(Model3DGroup group) {
+            if (group.Children.Count == 1)
+                return group.Children[0] as GeometryModel3D;
+            int maxCount = int.MinValue;
+            GeometryModel3D maxModel = null;
+            foreach (GeometryModel3D model in group.Children) {
+                int count = ((MeshGeometry3D)model.Geometry).Positions.Count;
+                if (maxCount < count) {
+                    maxCount = count;
+                    maxModel = model;
+                }
+            }
+            return maxModel;
+        }
+        private void Slice(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Cut my life into pieces");
         }
     }
 }
